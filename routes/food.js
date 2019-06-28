44e8996e3dc4f446883fc315d618e0919b9eb8e9
Yasura <yasuracreation@gmail.com>
2019-06-29 02:32:24 +0530

    food data status send with requres header

--- routes/food.js
+++ routes/food.js
@@ -6,7 +6,7 @@ router.get('/',async (req,res)=>{
     const food = await Food.find().sort('foodName');
     if(!food) return res.status('400').send('Food information not available');
     
-    res.send(food);
+    res.status(200).send(food);
 });
 
 router.post('/',async (req,res)=>{
@@ -27,7 +27,7 @@ router.post('/',async (req,res)=>{
 
     try{ 
         food = await food.save();
-        res.send(food);
+        res.status(200).send(food);
     }catch(ex){
         res.status(500)
         .send('Exeption while saving food, food havent created successfully '+ex.message);
